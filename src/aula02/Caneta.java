1df0251369d0317b90db9d88bc55555c4d35cc02
JFNascimento99 <jfnascimento99@gmail.com>
2022-06-13 18:48:39 -0300

    Aula 02 beginning

---
+++ src/aula02/Caneta.java
@@ -0,0 +1,22 @@
+package aula02;
+
+public class Caneta {
+	String modelo;
+	String cor;
+	String ponta;
+	int carga;
+	boolean tampada;
+	
+	void rabiscar() {
+		
+	}
+	
+	void tampar() {
+		
+	}
+	
+	void destampar() {
+		
+	}
+	
+}
