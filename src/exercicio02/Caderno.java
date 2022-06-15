c2a1d5696c044575fac4bd2f4b2c813693262300
JFNascimento99 <jfnascimento99@gmail.com>
2022-06-15 18:50:18 -0300

    Finshed fist part of exercice 02

--- src/exercicio02/Caderno.java
+++ src/exercicio02/Caderno.java
@@ -1,16 +1,35 @@
 package exercicio02;
 
 public class Caderno {
-	boolean riscado;
-	boolean aberto;
+	boolean riscado = false;
+	boolean aberto = false;
 	int folhas;
 	String modelo;
-	
 	
 	void status() {
 		System.out.println("Temos um " + this.modelo);
 		System.out.println("Com "+ this.folhas + " folhas");
 		System.out.println("Ele está aberto? " + this.aberto);
 		System.out.println("Ele está riscado? " + this.riscado);
+	}
+	
+	void riscar() {
+		if (this.aberto != false) {
+			if(this.folhas > 0) {
+				this.riscado = true;
+			} else {
+				System.out.println("Não posso riscar sem folhas");
+			}
+		} else {
+			System.out.println("Não posso riscar se tiver fechado");
+		}
+	}
+	
+	void abrir() {
+		this.aberto = true; 
+	}
+	
+	void fechar() {
+		this.aberto = false;
 	}
 }
--- src/exercicio02/Construtor.java
+++ src/exercicio02/Construtor.java
@@ -3,6 +3,20 @@ package exercicio02;
 public class Construtor {
 
 	public static void main(String[] args) {
+		Caderno c1 = new Caderno();
+		c1.modelo = "fichário";
+		c1.folhas = 15;
 		
+		c1.abrir();
+		c1.riscar();
+		c1.status();
+		
+		Caderno c2 = new Caderno();
+		c2.modelo = "caderno";
+		c2.folhas = 0;
+		
+		c2.abrir();
+		c2.status();
+		c2.riscar();
 	}
 }
