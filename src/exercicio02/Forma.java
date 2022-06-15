a7601fb07cb8d67498fde48963400cdc772ed472
JFNascimento99 <jfnascimento99@gmail.com>
2022-06-15 19:13:46 -0300

    Started second part of exercice 02

---
+++ src/exercicio02/Forma.java
@@ -0,0 +1,12 @@
+package exercicio02;
+
+public class Forma {
+	String sabor;
+	String cobertura;
+	String formato;
+	int tamanho;
+	float preço;
+	boolean noForno = false;
+	boolean assado = false;
+	boolean queimado = false;
+}
