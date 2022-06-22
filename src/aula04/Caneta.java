823e37fbe4362a646893d3794ecfb6da3e0688fb
JFNascimento99 <jfnascimento99@gmail.com>
2022-06-22 18:55:25 -0300

    Starting exercices Aula04

---
+++ src/aula04/Caneta.java
@@ -0,0 +1,14 @@
+package aula04;
+
+public class Caneta {
+	public String modelo;
+	private float ponta;
+	
+	public String getModelo() {
+		return this.modelo;
+	}
+	
+	public void setModelo(String m) {
+		
+	}
+}
